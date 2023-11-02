84be38cddab17ec9ba49f6c4acb239ee88dc0542
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-02 08:11:02 +0700

    add responsiveness, framer-motion- and clsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { IconQuote, IconBrandTwitterFilled } from "@tabler/icons-react";
+import clsx from "clsx";
 
 const Card = ({ author, content, handleClick }) => {
   const encoded =
@@ -9,13 +10,21 @@ const Card = ({ author, content, handleClick }) => {
   return (
     <div
       id="quote-box"
-      className="flex flex-col w-[500px] min-h-[250px] justify-center items-center rounded-lg border p-4 gap-2"
+      className={clsx(
+        "flex flex-col w-[80vw] min-h-[250px] justify-center items-center rounded-lg border p-4 gap-2 relative text-md",
+        "sm:w-[500px]"
+      )}
     >
-      <div id="text" className="text-center text-2xl">
+      <div id="text" className="text-center sm:text-2xl">
         <IconQuote
-          size={40}
+          size={20}
           fill="black"
-          className="inline transform -scale-x-100"
+          className="inline transform -scale-x-100 sm:hidden"
+        />
+        <IconQuote
+          size={35}
+          fill="black"
+          className="sm:inline transform -scale-x-100 hidden"
         />
         {content}
       </div>
@@ -24,13 +33,13 @@ const Card = ({ author, content, handleClick }) => {
       </div>
       <button
         id="new-quote"
-        className="bg-black text-white font-bold py-1 px-5 rounded-md"
+        className="bg-black text-white text-sm sm:text-md font-bold py-1 px-5 rounded-md relative top-[15px]"
         onClick={handleClick}
       >
         New Quote
       </button>
       <a
-        className="self-end"
+        className="absolute bottom-5 right-5"
         id="tweet-quote"
         title={"Tweet this quote!"}
         target="_top"
